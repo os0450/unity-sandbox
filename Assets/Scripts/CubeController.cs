8f30b7834f0e4e473b370943f80ba58ddc1a3215
os0450 <o.shiko.0450@gmail.com>
2013-12-16 23:35:05 +0900

    modify script

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
